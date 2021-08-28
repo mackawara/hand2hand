1f510bd2cb41f66b5e5a83e60fb06870a250f5d2
mackawara <mkawara@outlook.com>
2021-08-28 07:59:58 +0200

    add MongoDB

--- public/index.js
+++ public/index.js
@@ -1,7 +1,155 @@
-/* var navbarToggler=document.getElementById("navbarToggler");
-var navbar=document.getElementById("mainNavbar");
-navbarToggler.addEventListener('click',toggleNavbar())
-function toggleNavbar() {
-navbar.classList.toggle('active')
-    
-} */
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM loaded");
+});
+  var form;
+  function assign() {
+    console.log("testing");
+    form = document.getElementById("form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      console.log("event registered");
+      validationResult();
+    });
+  }
+
+  function inputValidation(inputsField) {
+    this.inputsField = inputsField;
+    const small = inputsField.parentElement.querySelector("small");
+
+    this.setError = function () {
+      inputsField.parentElement.className = "form-control error";
+    };
+    this.empty = function () {
+      small.innerText = inputsField.name + " cannot be empty";
+    };
+    this.invalid = function () {
+      small.innerText = " Please enter a valid " + inputsField.name;
+    };
+    this.setSuccess = function () {
+      //small.innerText = message;
+      inputsField.parentElement.className = "form-control success";
+    };
+  }
+  function validationResult() {
+    const email = document.getElementById("email"); //.value
+    const inputErrors = [];
+    let emailField = new inputValidation(email);
+
+    if (
+      /(?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$)/.test(
+        email.value
+      ) === false
+    ) {
+      emailField.setError();
+      emailField.invalid();
+      console.log("testInvalidError");
+      inputErrors.push(email.name);
+    } else {
+      emailField.setSuccess();
+      console.log("testSuccess");
+    }
+
+    const fullname = document.getElementById("fullname"); //value;
+
+    let nameField = new inputValidation(fullname);
+    if (
+      fullname.value.length > 35 ||
+      /^[a-z ,.'-]+$/i.test(fullname.value) === false
+    ) {
+      nameField.setError();
+      nameField.invalid();
+      inputErrors.push(fullname.name);
+    } else {
+      nameField.setSuccess();
+    }
+
+    const mobileNumber = document.getElementById("mobileNumber");
+    let mobileNumberField = new inputValidation(mobileNumber);
+    if (mobileNumber.value.length > 13 || mobileNumber.value.length < 10) {
+      mobileNumberField.setError();
+      mobileNumberField.invalid();
+      inputErrors.push(mobileNumber.name);
+    } else {
+      mobileNumberField.setSuccess();
+    }
+
+    const subject = document.getElementById("subject");
+    let subjectField = new inputValidation(subject);
+    if (subject.value.length > 60) {
+      subjectField.setError();
+      subjectField.invalid();
+      inputErrors.push(subject.name);
+    } else {
+      subjectField.setSuccess();
+    }
+
+    const text = document.getElementById("text"); //value;
+    let textField = new inputValidation(text);
+    if (text.value.length > 500) {
+      textField.setError();
+      textField.invalid();
+      inputErrors.push(textField.name);
+    }
+
+    form = document.getElementById("form");
+    const inputs = form.querySelectorAll("input");
+
+    for (let i = 0; i < inputs.length; i++) {
+      const input = inputs[i];
+
+      let inputField = new inputValidation(input);
+      if (input.value === "") {
+        inputField.setError();
+        inputField.empty();
+        console.log("error empty");
+        inputErrors.push(input.name);
+      } else {
+        console.log("no Empty filed");
+      }
+    }
+    console.log(inputErrors);
+    if (inputErrors.length === 0) {
+      postMail();
+    }
+    /* if (email.value === "") {
+    emailField.setError();
+    emailField.empty();
+    console.log("Email is empty");
+  } else if (
+    /(?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$)/.test(
+      email.value
+    ) === false
+  ) {
+    emailField.setError();
+    emailField.invalid();
+    console.log("Some errors were found");
+  } else {
+    emailField.setSuccess();
+    console.log("the input passed the validation");
+    postMail();
+  } */
+  }
+  function postMail() {
+    console.log("send");
+    form = document.getElementById("form");
+
+    //const formObject = Object.form;
+    const maildata = new FormData(form);
+
+    sendMail(maildata); //send the FormData we have created
+    function sendMail(maildata) {
+      console.log("sendmail Working");
+      fetch("http://localhost:3000/send", {
+        method: "POST",
+        body: maildata,
+      }).then((response) => {
+        console.log("server has received our request");
+      });
+    }
+
+    //validationResult();
+    /* sendMail(maildata); */
+    console.log(maildata);
+  }
+
--- server.js
+++ server.js
@@ -4,53 +4,128 @@ var path = "path";
 var bodyParse = "body-parser";
 const os = require("os");
 const totalMemory = os.totalmem;
+const cors = require("cors");
 
 const nodemailer = require("nodemailer");
 const multiparty = require("multiparty");
 const { request } = require("https");
 require("dotenv").config();
 
+const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT;
+const { check, validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+const { error, timeStamp } = require("console");
+
+const uri =
+  "mongodb+srv://mkawara:uyaoDLELgsBnSHud2015@hand2hand.4pnao.mongodb.net/customerFeedback?retryWrites=true&w=majority";
+
+const databaseName = "customerFeedback";
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbname: databaseName,
+};
+mongoose
+  .connect(uri, options, console.log("database connected"))
+  .then((result) =>
+    app.listen(port, () => {
+      console.log(`server listening at port:${port}`);
+    })
+  )
+  .catch((err) => console.log(err));
+
+const database = mongoose.connection;
+database.on("error", console.error.bind(console, "connection error:"));
+database.once("open", function () {
+  console.log("We are connected");
+});
+
+console.log(check("ëmail.com").isEmail);
+const feedbackSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength:[2, `please enter a valid name`],
+    maxlength:[30, `please enter a valid name`],
+    match:[/^[a-z ,.'-]+$/ , " Please enter a valid name"]
+  },
+  subject: {
+    type: String,
+    required: true,
+    maxlength:[30, "Please keep Subject to within 30 characters"] 
+  },
+  text: {
+    type: String,
+    required: [true, "Please enter text"],
+    maxlength:[350, "Please keep Subject to within 30 characters"],
+
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    validate:{
+      validator :function(v){
+       check(v).isEmail}
+    }
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+  },
+});
+
+const Model = mongoose.model("feedback", feedbackSchema);
+
+const newListing = {
+  detail: {
+    name: "Lofty",
+    rooms: 2,
+    bedrooms: 1,
+  },
+};
+/* 
+client.connect((err) => {
+  
+  collection
+    .insertOne(newListing, () => {
+      //console.log(collection);
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("document detail");
+      }
+    })
+    .then(() => {
+      const query = {};
+      collection.find(query);
+      console.log(collection);
+    });
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(client.db);
+  }
+}); */
+
+const query = { name: "rating", collection: "listingsAndReviews" }; // this is the query document , specifies the query parameters
+
+//npc nodeclient.close();
+//client.connect();
 app.use(
   express.urlencoded({
     extended: false,
   })
 );
-//app.use(express.json());
+app.use(express.json());
 /* const EventEmitter = require("events"); // creates events emitter class
 const emitter = new EventEmitter(); // creates an instance of the event emitter class
  */
-var server = app.listen(port, () => {
-  console.log(`Listening at port ${port}`);
-});
-app.get("/search/product", displayData, showMore);
-function displayData(req, res, next) {
-  res.send("Search for prodct");
-  console.log("display");
-  next();
-}
-function showMore(req, res, next) {
-  console.log("show more");
-}
-app.get("/contactus", (req, res) => {
-  res.sendFile(process.cwd() + "/public/contactus.html");
-  console.log("contactus");
-});
 
-app.post("/send", (req, res) => {
-  let data = req.body;
-  console.log(data, "this works");
-  res.send(data);
-  /* data.parse(req, function (err, fields) {
-    console.log(data);
-  }); */
-});
+app.use(express.static(__dirname + "/public"));
 
-/* app.use(express.static("public"), logger);
-function logger() {
-  console.log("logger working");
-} */
 const transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com", //replace with your email provider
   port: 587,
@@ -59,55 +134,90 @@ const transporter = nodemailer.createTransport({
     pass: process.env.PASS,
   },
 });
-
-// verify connection configuration
-transporter.verify(function (error, success) {
+// verify if nodemailer is authorized to send emails from that address.
+transporter.verify(function (error, success, next) {
   if (error) {
     console.log(error);
   } else {
-    console.log("Server is ready to take our messages");
+    console.log("transorter has verified an is ready to send messages");
   }
 });
-/* 
-app.post("/public/contactus.html", (req, res) => {
-  res.
+
+app.post("/send", (req, res, next) => {
+  console.log("received");
+
   let form = new multiparty.Form();
   let data = {};
+
   form.parse(req, function (err, fields) {
-    console.log(fields);
     Object.keys(fields).forEach(function (property) {
       data[property] = fields[property].toString();
     });
-    console.log(data);
+
+  const fullname=data.fullname
+  const email=data.email;
+  const mobileNumber=data.mobileNumber
+  const subject=data.subject;
+  const text=data.text;
+
+  //input validation using Express validato
+  
+    const feedback = new Model({
+      name: fullname,
+      email: email,
+      subject: subject,
+      text:text,
+      mobileNumber:mobileNumber
+    });
+
+    feedback.save()
+  
+
     const sender = process.env.EMAIL;
     const receiver = process.env.RECEIVER;
-    console.log(sender, receiver);
     const mail = {
-      sender: sender,
-      to: receiver, // receiver email,
+      from: sender,
+      to: receiver,
       subject: data.subject,
-      text: `${data.name} <${data.email}> \n${data.message}`,
+      text: `From : ${data.fname} ${data.lastname} \n Email: <${data.email}>  \n Mobile Number:${data.mobileNumber}  \n Feedback: ${data.text}`,
     };
 
-    transporter.sendMail(mail, (err, data) => {
+    transporter.sendMail(mail, (err, res, data) => {
       if (err) {
-        console.log(err);
-        res.status(500).send("Something went wrong.");
+        console.error(err);
+        // res.status(500).send("Something went wrong.");
       } else {
         res.status(200).send("Email successfully sent to recipient!");
+        res.send(`email has been sent succesfully to ${receiver}`);
+        
       }
-    });
-  });
+    })
+  })
 });
- */
+function userInputValidator(req, res, next) {
+ 
+ check(fullname).contains()
+  const errors = check(req);
+  if (!errors.isEmpty) {
+    console.log(`there was an error ${errors}`);
+    console.log("there was an error");
+    return;
+  } else {
+    res.send("Email was sent succesfully");
+    console.log("there was no error");
+    next();
+  }
+}
+const {checkSchema}= require ("express-validator");
 
-app.get("/contactus.html", (req, res) => {
+
+/* app.get("/contactus.html", (req, res) => {
   let data = req.body;
   data;
   res.sendFile(process.cwd() + "/public/contactus.html");
 });
-
-app.route("/").get(function (req, res) {
+ */
+/* app.route("/").get(function (req, res) {
   //make the contact page the the first page on the appv
   res.sendFile(process.cwd() + "/public/contactus.html"); // The process.cwd() method returns the current working directory of the Node.js process.
-});
+}); */
